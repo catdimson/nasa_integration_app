e1ae354eeeb43e4e5ed20353e4a5c1fc1eb6990d
Dmitry Kotik <k-work31@yandex.ru>
2022-02-27 20:28:37 +0300

    lesson1 Знакомство к CoordinatorLayout

--- app/src/main/java/ru/dkotik/nasaintegrationapp/view/main/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/dkotik/nasaintegrationapp/view/main/PictureOfTheDayFragment.kt
@@ -1,13 +1,18 @@
 package ru.dkotik.nasaintegrationapp.view.main
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.api.load
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import ru.dkotik.nasaintegrationapp.databinding.FragmentMainBinding
 import ru.dkotik.nasaintegrationapp.viewmodel.PictureOfTheDayDataState
 import ru.dkotik.nasaintegrationapp.viewmodel.PictureOfTheDayViewModel
@@ -25,6 +30,8 @@ class PictureOfTheDayFragment: Fragment() {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -40,6 +47,31 @@ class PictureOfTheDayFragment: Fragment() {
             renderData(it)
         })
         viewModel.sendServerRequest()
+
+        binding.inputLayout.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
+            })
+        }
+
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.bsl.bottomSheetContainer)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    /*BottomSheetBehavior.STATE_DRAGGING -> TODO("not implemented")
+                    BottomSheetBehavior.STATE_COLLAPSED -> TODO("not implemented")
+                    BottomSheetBehavior.STATE_EXPANDED -> TODO("not implemented")
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> TODO("not implemented")
+                    BottomSheetBehavior.STATE_HIDDEN -> TODO("not implemented")
+                    BottomSheetBehavior.STATE_SETTLING -> TODO("not implemented")*/
+                }
+            }
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Log.d("mylogs", "$slideOffset slideOffset")
+            }
+        })
     }
 
     private fun renderData(data: PictureOfTheDayDataState) {
@@ -56,6 +88,8 @@ class PictureOfTheDayFragment: Fragment() {
                     //showSuccess()
                     //Coil в работе: достаточно вызвать у нашего ImageView
                     //нужную extension-функцию и передать ссылку и заглушки для placeholder
+                    binding.bsl.bottomSheetDescriptionHeader.text = serverResponseData.title
+                    binding.bsl.bottomSheetDescription.text = serverResponseData.explanation
                     binding.imageView.load(urlImage)
                 }
             }
