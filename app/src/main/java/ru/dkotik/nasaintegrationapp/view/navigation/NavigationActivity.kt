d8ec56299defb19c87a863fbe261b1f756e351e5
Dmitry Kotik <k-work31@yandex.ru>
2022-03-10 23:38:10 +0300

    lesson Добавлены кастомные вьюшки для табов

--- app/src/main/java/ru/dkotik/nasaintegrationapp/view/navigation/NavigationActivity.kt
+++ app/src/main/java/ru/dkotik/nasaintegrationapp/view/navigation/NavigationActivity.kt
@@ -1,5 +1,6 @@
 package ru.dkotik.nasaintegrationapp.view.navigation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.dkotik.nasaintegrationapp.R
@@ -9,6 +10,7 @@ class NavigationActivity: AppCompatActivity() {
     
     lateinit var binding: ActivityNavigationBinding
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNavigationBinding.inflate(layoutInflater)
@@ -16,9 +18,13 @@ class NavigationActivity: AppCompatActivity() {
         binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
         binding.tabLayout.setupWithViewPager(binding.viewPager)
 
-        binding.tabLayout.getTabAt(EARTH_KEY)?.setIcon(R.drawable.ic_earth)
+        /*binding.tabLayout.getTabAt(EARTH_KEY)?.setIcon(R.drawable.ic_earth)
         binding.tabLayout.getTabAt(MARS_KEY)?.setIcon(R.drawable.ic_mars)
-        binding.tabLayout.getTabAt(SYSTEM_KEY)?.setIcon(R.drawable.ic_system)
+        binding.tabLayout.getTabAt(SYSTEM_KEY)?.setIcon(R.drawable.ic_system)*/
+
+        binding.tabLayout.getTabAt(EARTH_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_earth)
+        binding.tabLayout.getTabAt(MARS_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_mars)
+        binding.tabLayout.getTabAt(SYSTEM_KEY)?.setCustomView(R.layout.activity_navigation_tablayout_item_system)
     }
     
 }
