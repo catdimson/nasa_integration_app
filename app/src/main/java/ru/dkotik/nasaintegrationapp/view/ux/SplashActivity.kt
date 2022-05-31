8330aada2c78b7b34f5b983475ef19941e28777a
Kotik <k-work31@yandex.ru>
2022-05-31 19:23:17 +0300

    lesson8 Добавлен продвинутый загрузочный экран

---
+++ app/src/main/java/ru/dkotik/nasaintegrationapp/view/ux/SplashActivity.kt
@@ -0,0 +1,38 @@
+package ru.dkotik.nasaintegrationapp.view.ux
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import ru.dkotik.nasaintegrationapp.R
+import ru.dkotik.nasaintegrationapp.view.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.SecondaryTheme)
+        setContentView(R.layout.activity_splash)
+
+        findViewById<ImageView>(R.id.iv).animate().rotationBy(720f).setDuration(5000L).start()
+
+        /*Handler(mainLooper).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 5000L)*/
+
+        val cdt = object: CountDownTimer(2000,2000){
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+        }
+        cdt.start()
+
+    }
+}
